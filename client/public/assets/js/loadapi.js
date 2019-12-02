de5aa5268a500df6deecf544bf19c19d61a09074
l-yang-05 <40499685+l-yang-05@users.noreply.github.com>
2019-12-02 10:29:01 -0500

    Testing to cache products api

--- client/public/assets/js/loadapi.js
+++ client/public/assets/js/loadapi.js
@@ -2,56 +2,10 @@ loadProducts()
 let dataArray = [];
 
 function loadProdcuts() {
-    fetch("/api/products")
+    fetch("https://kiwisparadise-pwa.herokuapp.com/api/products")
         .then(res => res.json())
         .then(data => {
-            dataArray = data;
-            createCards(dataArray);
+            console.log(data)
         });
 }
 
-function filterType(type) {
-    fetch(`/api/productfilter?type=${type}`)
-        .then(res => res.json())
-        .then(data => {
-            dataArray = data;
-            createCards(dataArray);
-        });
-}
-
-function filterPrice(price) {
-    fetch(`/api/productfilter?price=${price}`)
-        .then(res => res.json())
-        .then(data => {
-            dataArray = data;
-            createCards(dataArray);
-        });
-}
-
-function filterBoth(type, price) {
-    fetch(`/api/productfilter?type=${type}&price=${price}`)
-        .then(res => res.json())
-        .then(data => {
-            dataArray = data;
-            createCards(dataArray);
-        });
-}
-
-function createCards(data) {
-    const container = document.getElementsByClassName("container")[0];
-    container.innerHTML = "";
-    let lastRow;
-    const row = createEl("div", "row");
-
-    return data.forEach(function (image, index) {
-        const col = createEl("div", "col-md-4 mt-4");
-        col.appendChild(createCard(image));
-        if (index % 3 === 0) {
-            row.appendChild(col);
-            container.appendChild(row);
-            lastRow = row;
-        }
-
-        return lastRow.appendChild(col);
-    });
-}
